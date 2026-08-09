628623c8d57f57524ca27218c24eaa75bd6eb8d8
fuivu <2809957339@qq.com>
2026-08-09 14:40:42 +0800

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // MediaTek NeuroPilot SDK (if available locally)
+        // maven { url = uri("${rootProject.projectDir}/libs/maven") }
+    }
+}
+
+rootProject.name = "LocalAIPainter"
+include(":app")
